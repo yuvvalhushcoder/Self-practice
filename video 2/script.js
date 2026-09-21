60159ab569a59c53c5a33bb05549a6ce0a379d6a
Yuvval <yuvvalbhasin.3010@gmail.com>
2026-09-21 10:42:05 +0530

    basic code for html java css

---
+++ video 2/script.js
@@ -0,0 +1,5 @@
+alert('Hello, Welcome to my Website!');
+alert('I am Yuvval Bhasin, a Web Developer and Designer.');
+alert('I have created this website to showcase my skills and projects.');
+alert('I hope you enjoy exploring my website and find it informative.');
+alert('Feel free to contact me if you have any questions or feedback. Thank you for visiting!');
